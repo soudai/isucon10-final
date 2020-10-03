3eeed0e70e1900b19bd4864a4061a47c3fa87d88
Ryuta Kamizono <kamipo@gmail.com>
2020-10-03 11:01:52 +0900

    fix

--- ruby/app.rb
+++ ruby/app.rb
@@ -12,23 +12,6 @@ require 'routes'
 require 'database'
 require 'notifier'
 
-class Mysql2ClientWithNewRelic < Mysql2::Client
-  def initialize(*args)
-    super
-  end
-
-  def query(sql, *args)
-    callback = -> (result, metrics, elapsed) do
-      NewRelic::Agent::Datastores.notice_sql(sql, metrics, elapsed)
-    end
-    op = sql[/^(select|insert|update|delete|begin|commit|rollback)/i] || 'other'
-    table = sql[/\bbenchmark_jobs|clarifications|contest_config|contestants|notifications|push_subscriptions|teams\b/] || 'other'
-    NewRelic::Agent::Datastores.wrap('MySQL', op, table, callback) do
-      super
-    end
-  end
-end
-
 module Xsuportal
   class App < Sinatra::Base
     include Xsuportal::Routes
--- ruby/lib/database.rb
+++ ruby/lib/database.rb
@@ -1,6 +1,23 @@
 require 'mysql2'
 require 'mysql2-cs-bind'
 
+class Mysql2ClientWithNewRelic < Mysql2::Client
+  def initialize(*args)
+    super
+  end
+
+  def query(sql, *args)
+    callback = -> (result, metrics, elapsed) do
+      NewRelic::Agent::Datastores.notice_sql(sql, metrics, elapsed)
+    end
+    op = sql[/^(select|insert|update|delete|begin|commit|rollback)/i] || 'other'
+    table = sql[/\bbenchmark_jobs|clarifications|contest_config|contestants|notifications|push_subscriptions|teams\b/] || 'other'
+    NewRelic::Agent::Datastores.wrap('MySQL', op, table, callback) do
+      super
+    end
+  end
+end
+
 module Xsuportal
   class Database
     class << self
